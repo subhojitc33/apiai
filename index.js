f2d48a887fbb795c9c82a5affb4ba2c0fb2c81cb
subhojitc33 <subhojitc3@gmail.com>
2017-07-30 03:45:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,13 @@ console.log('>>>'+requestBody.result.parameters.SearchtermText);
                      conn.apex.post("/analyzeRequest/", body, function(errv,resvar)  {
                     if (errv) {  console.error(errv); }
                     console.log("total : " + JSON.stringify(resvar));
-                        speech+=resvar;
+                      for(var i=0;i<resvar.response.size();i++){
+                          var feedItemId=resvar.response[i].feeditem.Id;
+                          var feedItemBody=resvar.response[i].feeditem.Body;
+                          //var content=
+                        speech+='<b> hii </b>';
+                         
+                      } 
                         
                         return res.json({
             speech: speech,
